2000c1075c70941554a531c7cf74ceb7e6e9a032
Mikael Amaral <mikael.a.amaral07@gmail.com>
2024-08-12 16:33:53 -0300

    Add project files.

---
+++ Data/AulaContext.cs
@@ -0,0 +1,13 @@
+﻿using AulaDeASPNet.Models;
+using Microsoft.EntityFrameworkCore;
+namespace AulaDeASPNet.Data
+{
+    public class AulaContext : DbContext
+    {
+        //public AulaContext(DbContextOptions options) : base(options)
+        public AulaContext(DbContextOptions<AulaContext> options) : base(options)
+        {
+        }
+        public DbSet<AulaDeASPNet.Models.Cliente> Clientes { get; set; } = default!;
+    }
+}
